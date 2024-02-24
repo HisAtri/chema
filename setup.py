80305c6351866ca701fe0e76c8884644d0deef99
HisAtri <yz@ghacg.com>
2024-02-25 01:01:31 +0800

    更新版本号

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name="chemax",
-    version="1.0.0",
+    version="1.0.1",
     author="HisAtri",
     author_email="ylsymc@outlook.com",
     description="Easily calculate the m.w. and e.m of a molecule",
